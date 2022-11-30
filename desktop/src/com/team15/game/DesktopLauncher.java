d489775533be3ef022ed5a5f23e744a863860d3f
E-Gruff <ethangriffiths@protonmail.com>
2022-11-30 17:48:52 +0000

    Empty project

---
+++ desktop/src/com/team15/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.team15.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.team15.game.Team15Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Team 15 project");
+		new Lwjgl3Application(new Team15Game(), config);
+	}
+}
